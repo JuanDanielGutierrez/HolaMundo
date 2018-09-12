b5813451083f2ffc45101127303c86e75f05f0bc
Daniel <danielcore5@hotmail.com>
2018-09-11 20:34:23 -0500

    Agregar archivos de proyecto.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+            Console.ReadKey();
+        }
+
+        
+        public static String CreateMessage()
+        {
+            int suma = 2 + 3;
+            return "Hola Mundo "+suma;
+        }
+    }
+}
---
+++ PruebaHola/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PruebaHola;
+using HolaMundo;
+
+namespace PruebaHola
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hola Mundo", Program.CreateMessage());
+        }
+    }
+}
